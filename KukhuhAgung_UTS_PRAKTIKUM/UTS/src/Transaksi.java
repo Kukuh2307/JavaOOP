487efd8a62524980063a09aae3264eecd06c401d
kukuh2307 <kukuhagung12@gmail.com>
2022-11-01 12:04:36 +0700

    menambahkan file uts asal jadi

---
+++ KukhuhAgung_UTS_PRAKTIKUM/UTS/src/Transaksi.java
@@ -0,0 +1,23 @@
+public class Transaksi extends KafeKopi {
+    int nomorMeja,harga;
+    String pesan;
+    Transaksi(int nomorMeja){
+        this.nomorMeja = nomorMeja;
+    }
+    public void infoTransaksi(){
+        super.tampilInfoKafe();
+        super.tampilPisangCoklat();
+        super.tampilPukisKeju();
+        super.tampilMocacino();
+        hitungTotalHarga();
+    }
+    public void hitungTotalHarga(){
+        harga = super.pisangCoklat(3) + super.pukisKeju(2) + super.mocacino(3);
+        System.out.println(harga);
+
+    }
+
+    
+}
+
+
---
+++ KukhuhAgung_UTS_PRAKTIKUM/UTS/src/UTSP020.java
@@ -0,0 +1,9 @@
+public class UTSP020 {
+
+    public static void main(String[] args) {   
+        Transaksi pembeli1 = new Transaksi(2);
+        pembeli1.hitungTotalHarga();
+        pembeli1.infoTransaksi();
+    }
+
+}
